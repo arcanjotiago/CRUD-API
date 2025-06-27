7a580f5f5221b0164f2f591cc4c9891f331aa12a
arcanjotiago <tiagoarcanjom@gmail.com>
2025-06-27 17:15:45 -0300

    code refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import router  from "./controllers/user.controller";
 import express from 'express';
-import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true,}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true,}));
 
 app.use("/", router);
 app.listen(port, () => {console.log(`App listening on port ${port}`)});
